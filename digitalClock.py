3c7b71f9585f654debd1e76658641690b0168b9a
zybixx84 <zybixx@yahoo.co.uk>
2023-08-26 20:03:45 +0100

    Code modified

--- digitalClock.py
+++ digitalClock.py
@@ -3,5 +3,9 @@ from tkinter import Tk, Label
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
+window.configure(bg="steelblue")
+
+label = Label(window, text="Welcome!", font=("Arial Black",78,"bold"), gb="steelblue", fg="white")
+label.pack(pady=100)
 
 window.mainloop()
