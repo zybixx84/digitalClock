12f2b7f5aa22557b5e593b43398855e4fdf5e8b0
zybixx84 <zybixx@yahoo.co.uk>
2023-08-26 20:05:24 +0100

    Error fixed

--- digitalClock.py
+++ digitalClock.py
@@ -5,7 +5,7 @@ window.title("Digital Clock")
 window.geometry("600x300")
 window.configure(bg="steelblue")
 
-label = Label(window, text="Welcome!", font=("Arial Black",78,"bold"), gb="steelblue", fg="white")
+label = Label(window, text="Welcome!", font=("Arial Black",78,"bold"), bg="steelblue", fg="white")
 label.pack(pady=100)
 
 window.mainloop()
